1c327325ade69c99865fc0efeda52f9f6c020bd4
klevari <klevari@apple.com>
2021-12-23 10:03:32 -0800

    Homepage 2.0

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "DIY - Homepage with Jekyll & Minimal Mistakes in GitHub.",
+        "excerpt":"In this post I’ll share how I built this website, i.e., one that is hosted in GitHub Pages, and powered by Jekyll &amp; Minimal Mistakes. I think that it can be done by anyone that is enthusiastic about learning new technologies (actually, a keyboard, a screen, and a connection to...","categories": [],
+        "tags": [],
+        "url": "/CreatingAWebSite/",
+        "teaser": "/assets/images/banner.png"
+      }]
