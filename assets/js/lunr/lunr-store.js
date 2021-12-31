de5518a6de2fe4aa2befb4163a460cfe1048c870
klevari <klevari@apple.com>
2021-12-30 21:40:03 -0800

    Fix a few typos

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "DIY - Homepage with Jekyll & Minimal Mistakes in GitHub.",
-        "excerpt":"In this post I’ll share how I built this website, i.e., one that is hosted in GitHub Pages, and powered by Jekyll &amp; Minimal Mistakes. I think that it can be done by anyone that is enthusiastic about learning new technologies (actually, a keyboard, a screen, and a connection to...","categories": [],
+        "excerpt":"In this post, I’ll share how I built this website, i.e., one that is hosted in GitHub Pages, and powered by Jekyll &amp; Minimal Mistakes. I think that it can be done by anyone that is enthusiastic about learning new technologies (actually, a keyboard, a screen, and a connection to...","categories": [],
         "tags": [],
         "url": "/CreatingAWebSite/",
         "teaser": "/assets/images/banner.png"
